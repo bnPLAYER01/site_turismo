2a667befeeac9ddc5cb46da6b062126a63a55328
bnPLAYER01 <162187057+bnPLAYER01@users.noreply.github.com>
2026-03-09 10:40:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,8 @@ def avaliar():
         print(f"Erro ao salvar: {e}")
         return jsonify({"erro": "Erro interno no servidor"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
